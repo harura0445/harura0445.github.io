fb7b32e3f3995a69c3b5dfbed94ee31cd71b9aeb
harura0445 <2805158188@qq.com>
2026-03-10 21:37:40 +0800

    Revert "deploy to custom domain"
    This reverts commit 3f1e982d0db129e63c392942373380ad88b9239d.

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,8 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://blog.harura0445.asia/",
+	site: "https://harura0445.github.io/",
+	base: "/",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
